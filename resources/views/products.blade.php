97447a207686e14c39a5156db70caaebdbba69db
Felipe <felipealencar.0014@gmail.com>
2023-10-19 17:34:45 -0300

    inicio do projeto

---
+++ resources/views/products.blade.php
@@ -0,0 +1,2 @@
+<h1>Página de Produtos</h1>
+<a href="/">Voltar para home</a>
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // posso passar dados, e puxar na view
+    $nome = 'Felipe';
+    $idade = 30;
+
+    // é possível mandar array também para o view
+    $arr = [10, 20, 30, 40, 50];
+    
+    $nomes = ['Matheus', 'Maria', 'João', 'José'];
+
+    return view('welcome',
+    [
+        'nome' => $nome,
+        'idade' => $idade,
+        'profissao' => "Programador",
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]);
+});
+
+Route::get('/contact', function () { // em get() posso definir qualquer nome
+    return view('contact'); // em view() tenho que definir o nome da view.blade.php
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
